e30830528e6a8f8c62a73dff9daf5cf5573bc66a
Lijn-B <140913104+Lijn-B@users.noreply.github.com>
2025-06-23 20:36:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({
+  headless: 'new',
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
   const page = await browser.newPage();
   await page.goto('https://cookidoo.be/foundation/nl-BE/explore', { waitUntil: 'networkidle2' });
 
